64207434deea3232fa91dc202bca03100fbe6c32
cesarsan8a <cesarm.sanchezo@gmail.com>
2020-08-19 17:09:29 +1000

    first commit

---
+++ src/components/Portfolio.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Nav from './Nav';
+
+class Portfolio extends Component {
+  render () {
+    return (
+      <Nav />
+    );
+  }
+}
+
+export default Portfolio;
