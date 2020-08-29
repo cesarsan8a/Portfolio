8ce40c5b3822b72e73cf6abb5bf78d6d76eb26f2
Cesar <cesarm.sanchezo@gmail.com>
2020-08-29 16:38:44 +1000

    char cycle

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import '../styles/Intro.css';
 
 const characteristics = [
@@ -8,26 +8,49 @@ const characteristics = [
   "Not a salad"
 ];
 
-const describeMe = function (adj) {
-  return adj[Math.round(Math.random() * 3)];
-}
 
-function Intro () {
-  return (
-    <div className="intro-container">
-      <div className="greeting">
-        <h1>I'm Cesar</h1>
-      </div>
+class Intro extends Component {
+  constructor() {
+    super();
+    this.state = {
+      adj: 0, // index of the characteristic to display
+    }
+  }
 
-      <div className="characteristics">
-        <h2>{ describeMe(characteristics) }</h2>
-      </div>
+  changeAdj = () => { // loop through the array of characteristics
+    if ( this.state.adj < (characteristics.length - 1) ) {
+      this.setState({ adj: this.state.adj + 1 });
+    } else {
+      this.setState({ adj: 0 });
+    }
+  }
 
-      <div className="intro-img-container">
-        <img src="https://via.placeholder.com/700x300"/>
+  startAdjChange = () => {
+    this.intervalId = setInterval(this.changeAdj, 3000);
+    console.log('interval');
+  }
+
+  componentDidMount() {
+    this.startAdjChange();
+  }
+
+  render () {
+    return (
+      <div className="intro-container">
+        <div className="greeting">
+          <h1>I'm Cesar</h1>
+        </div>
+
+        <div className="characteristics">
+          <h2>{ characteristics[this.state.adj] }</h2>
+        </div>
+
+        <div className="intro-img-container">
+          <img src="https://via.placeholder.com/700x300"/>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Intro;
