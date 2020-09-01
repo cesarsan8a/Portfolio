c07bb2f219c7ec799a250535c391c19cc44cb17c
Cesar <cesarm.sanchezo@gmail.com>
2020-09-01 16:51:36 +1000

    fixes

--- src/components/Home.js
+++ src/components/Home.js
@@ -28,18 +28,18 @@ class Home extends Component {
 
             <div className="profile-about">
               <div className="profile-about-p-cont">
-                <h2>Hi</h2>
+                <h2>Hi.</h2>
                 <h3>I’m a web developer based in Melbourne, Australia</h3>
                 <p>I have a passion for backend development, and I love to build great code that makes <span id="nerd">nerdy</span> cool stuff happen.</p>
               </div>
               <div className="can-do">
                 <h2>What can I do?</h2>
                 <h3>I can design what you want</h3>
-                <p>I enjoy figuring out how to bring ideas to life using code. I have experience developing across using HTML, CSS, JavaScript, Ruby on Rails and React. Check out my <Link to="/work">projects</Link> for further details.</p>
+                <p>I enjoy figuring out how to bring ideas to life using code. I have experience developing with HTML, CSS, JavaScript, Ruby on Rails and React. Check out my <Link to="/work">projects</Link> to see examples of my work.</p>
               </div>
 
               <div className="can-help">
-                <h2>I can help</h2>
+                <h2>I can help.</h2>
                 <h3>I’m currently available for work</h3>
                 <p>I have a curious and detail-oriented attitude towards coding, and I love contributing to a team to make big things possible. If you have a product or project that needs a fast-paced learner with up-to-date training across a variety of languages, I’m your guy.</p>
               </div>
