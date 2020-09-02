b4912dcfe8a0b01b653cb00e34087091cde05839
Cesar <cesarm.sanchezo@gmail.com>
2020-09-02 12:24:00 +1000

    typo

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,7 +12,7 @@ class Contact extends Component {
 
         <div className="contact-text">
           <h3>Say Hello</h3>
-          <p>IIf you’re interested in talking more, I’d love to hear from you. Feel free to contact me at</p>
+          <p>If you’re interested in talking more, I’d love to hear from you. Feel free to contact me at</p>
           <h4>cesarm.sanchezo@gmail.com</h4>
           <p>or <a href="https://www.linkedin.com/in/cesarsanchez0ch04/" target="_blank" rel="noopener noreferrer">LinkedIn</a>. Mucho gusto!</p>
         </div>
