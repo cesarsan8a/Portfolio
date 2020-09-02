d789ee6b7d2c13c24615a79f530f92f7a74d75fd
Cesar <cesarm.sanchezo@gmail.com>
2020-09-02 18:27:36 +1000

    email

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -13,7 +13,7 @@ class Contact extends Component {
         <div className="contact-text">
           <h3>Say Hello</h3>
           <p>If you’re interested in talking more, I’d love to hear from you. Feel free to contact me at</p>
-          <h4>cesarm.sanchezo@gmail.com</h4>
+          <h4><a href="mailto:cesarm.sanchezo@gmail.com">cesarm.sanchezo@gmail.com</a></h4>
           <p>or <a href="https://www.linkedin.com/in/cesarsanchez0ch04/" target="_blank" rel="noopener noreferrer">LinkedIn</a>. Mucho gusto!</p>
         </div>
       </div>
